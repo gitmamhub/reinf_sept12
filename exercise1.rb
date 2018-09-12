6613743c20573efd0e8fac5dba3b912032e34920
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-12 16:04:42 -0400

    after rubocop

--- exercise1.rb
+++ exercise1.rb
@@ -1,12 +1,9 @@
 emotions = {
-
-:anger => rand(1..3),
-:sad   => rand(1..3),
-:happiness => rand(1..3),
-:fear => rand(1..3),
-:bordom => rand(1..3)
+  :anger => rand(1..3),
+  :sad => rand(1..3),
+  :happiness => rand(1..3),
+  :fear => rand(1..3),
+  :bordom => rand(1..3)
 }
 
-
-
-p emotions 
+p emotions
